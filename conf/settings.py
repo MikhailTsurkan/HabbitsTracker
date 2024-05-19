6048911d4e06135cd299e4b086f24e1544231bbf
Tsurkan <mikh.tsurkan@gmail.com>
2024-05-19 19:16:15 +0300

    add auth

--- conf/settings.py
+++ conf/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
 
     'users',
 ]
@@ -132,3 +134,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from users.apps import UsersConfig
 from users import views
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 
 app_name = UsersConfig.name
 
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
     path("registration/", views.RegistrationAPIVIew.as_view(), name="registration"),
+
 ]
