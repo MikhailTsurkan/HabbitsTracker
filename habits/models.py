c68fcbdf8936d0e3f0111dc2830011441cb7103d
Tsurkan <mikh.tsurkan@gmail.com>
2024-05-20 16:43:02 +0300

    add habit model with default fields

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,80 @@
-from django.db import models
+from datetime import timedelta
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+User = get_user_model()
+
+
+NULL = {
+    "null": True,
+    "blank": True
+}
+
+
+class Habit(models.Model):
+    """
+    Модель, описывающая привычку
+    """
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        help_text="создатель привычки",
+    )
+
+    place = models.CharField(
+        max_length=128,
+        verbose_name="Место",
+        help_text="место, в котором необходимо выполнять привычку",
+    )
+    perform_at = models.TimeField(
+        verbose_name="Время",
+        help_text="время, когда необходимо выполнять привычку",
+        **NULL
+    )
+    action = models.CharField(
+        max_length=256,
+        verbose_name="Действие",
+        help_text="действие, которое представляет собой привычка",
+    )
+    is_pleasant = models.BooleanField(
+        default=False,
+        verbose_name="Признак приятной привычки",
+        help_text="привычка, которую можно привязать к выполнению полезной привычки",
+    )
+    related_habit = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        verbose_name="Связанная привычка",
+        help_text="привычка, которая связана с другой привычкой, \
+важно указывать для полезных привычек, но не для приятных",
+        **NULL
+    )
+    periodicity = models.PositiveSmallIntegerField(
+        default=1,
+        verbose_name="Периодичность",
+        help_text="периодичность выполнения привычки для напоминания в днях",
+        **NULL
+    )
+    reward = models.CharField(
+        max_length=256,
+        verbose_name="Вознаграждение",
+        help_text="ем пользователь должен себя вознаградить после выполнения",
+        **NULL
+    )
+    perform_in = models.DurationField(
+        verbose_name="Время на выполнение",
+        help_text="время, которое предположительно потратит пользователь на выполнение привычки",
+    )
+    is_public = models.BooleanField(
+        default=False,
+        verbose_name="Признак публичности",
+        help_text="привычки можно публиковать в общий доступ, чтобы другие \
+пользователи могли брать в пример чужие привычки"
+    )
